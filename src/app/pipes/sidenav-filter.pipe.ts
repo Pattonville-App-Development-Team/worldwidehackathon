f934b007ac634a5bc0716ef9a168136cbb551670
Dom-1 <v.samuel88@yahoo.com>
2019-02-22 08:32:46 -0600

    attempted to figure out filtering

--- src/app/pipes/sidenav-filter.pipe.ts
+++ src/app/pipes/sidenav-filter.pipe.ts
@@ -1,15 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Item } from '../item';
 
 @Pipe({
   name: 'sidenavfilter',
   pure: false
 })
 export class SidenavFilterPipe implements PipeTransform {
-  transform(value: any, args?: any[]): any {
-    let result = value.filter( item => args.indexOf(item.season) > -1);
-    result = value.filter(item => args.indexOf(item.school) > -1);
-    result = value.filter(item => args.indexOf(item.size) > -1);
+  transform(array: Array<Item>, args?: string): Array<Item> {
+    let result = array;
+    // let result = array.filter((item => args.indexOf(item.category) > -1));
+    // result = array.filter(item => args.indexOf(item.age) > -1);
+    // result = array.filter(item => args.indexOf(item.size) > -1);
     return result;
   }
-
 }
